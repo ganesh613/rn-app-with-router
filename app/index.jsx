622585002bd2bb9284342f3ceaa0287565006ad9
Ganesh-Achala <kotanagaganesh.vasa@achalasolutions.com>
2025-02-13 08:47:56 +0530

    initial commit with Welcome section added

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import {View,ScrollView,SafeAreaView} from  'react-native'
+import { Stack,useRouter } from 'expo-router';
+import {COLORS,icons,images,SIZES} from '../constants'
+import { Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome } from '../components';
+
+const Home = () => { 
+
+    const router = useRouter()
+    const handleLeftClick = () =>{
+
+    }
+    const handleRightClick = () =>{
+
+    }
+
+    return (
+       <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: {backgroundColor:COLORS.lightWhite},
+                    headerTitleAlign:'center',
+                    headerTitle:'',
+                    headerShadowVisible:false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn 
+                            iconUrl={icons.menu} 
+                            dimension="60%" 
+                            handlePress={handleLeftClick}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn 
+                            iconUrl={images.profile} 
+                            dimension="100%"
+                            handlePress={handleRightClick}
+                        />
+                    )
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex:1,padding:SIZES.medium}}>
+                    <Welcome/>
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+       </SafeAreaView>
+    )
+}
+
+export default Home;
